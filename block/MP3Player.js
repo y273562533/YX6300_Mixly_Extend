5077a001cae6979e1113ca1b008c7953af8deb8d
y273562533 <273562533@qq.com>
2019-01-09 10:07:21 +0800

    YX6300
    mp3播放控制

---
+++ block/MP3Player.js
@@ -0,0 +1,95 @@
+'use strict';
+
+goog.provide('Blockly.Blocks.MP3Player');
+
+goog.require('Blockly.Blocks');
+
+
+var colorSet='#27b6ac';
+var mp3_Control =[
+                 ["播放", "mp3.play();"],
+                 ["暂停", "mp3.pause();"],
+                 ["停止", "mp3.stop();"],
+                 ["下一首", "mp3.playNext();"],
+                 ["上一首", "mp3.playPrevious();"],
+                 ["音量增", "mp3.volUp();"],
+                 ["音量减", "mp3.volDown();"],
+              ];
+
+Blockly.Blocks.mp3_begin={
+init:function(){
+    var PLAY_MODE =[["顺序播放", "mp3.play();"], 
+                      ["随机播放", "mp3.playRAN();"], 
+                      ["单曲循环", "mp3.playPL();"]
+                      ];
+    this.setColour(colorSet);
+    this.appendDummyInput("")
+        .appendField("播放模式");
+    this.appendDummyInput("")
+        .appendField(new Blockly.FieldDropdown(PLAY_MODE),"PLAY_MODE");
+    this.appendValueInput("Vol", Number)
+        .setCheck(Number)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("音量设置");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+  this.setInputsInline(true);
+  }
+};
+
+Blockly.Blocks.mp3_Setup = {
+  init: function() {
+   this.setColour(colorSet);
+   this.appendValueInput("RX", Number)
+       .appendField("MP3管脚设置")
+	     .appendField("RX#")
+       .setCheck(Number)
+	     .setAlign(Blockly.ALIGN_RIGHT);
+    this.appendValueInput("TX", Number)
+	   .appendField("TX#")
+     .setCheck(Number)
+	   .setAlign(Blockly.ALIGN_RIGHT);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+	this.setInputsInline(true);
+  }
+};
+Blockly.Blocks.mp3_control = {
+  init: function() {
+   this.setColour(colorSet);
+   this.appendDummyInput("")
+        .appendField("MP3播放控制");
+    this.appendDummyInput("")
+        .appendField(new Blockly.FieldDropdown(mp3_Control),"mp3_Control");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+  this.setInputsInline(true);
+  }
+};
+
+Blockly.Blocks.mp3_control2 = {
+  init: function() {
+   this.setColour(colorSet);
+   this.appendDummyInput("")
+        .appendField("播放mp3文件序号");
+    this.appendValueInput("file", Number)
+         .setCheck(Number)
+         .setAlign(Blockly.ALIGN_RIGHT);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+  this.setInputsInline(true);
+  }
+};
+
+/*Blockly.Blocks.mp3_PalyByName = {
+  init: function() {
+   this.setColour(colorSet);
+   this.appendValueInput('songName')
+       .setCheck(String)
+       .setAlign(Blockly.ALIGN_RIGHT)
+       .appendField("播放mp3文件名");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+  this.setInputsInline(true);
+  }
+};*/
---
+++ generator/MP3Player.js
@@ -0,0 +1,46 @@
+'use strict';
+
+goog.provide('Blockly.Arduino.MP3Player');
+
+goog.require('Blockly.Arduino');
+
+Blockly.Arduino.mp3_begin = function () {
+    var PLAY_MODE = this.getFieldValue('PLAY_MODE');
+    var setvol = Blockly.Arduino.valueToCode(this, 'Vol', Blockly.Arduino.ORDER_ATOMIC);
+    //Blockly.Arduino.setups_['mp3_begin'] =''+PLAY_MODE+'\n  mp3.setVol('+setvol+');'
+    var code=''+PLAY_MODE+'\nmp3.setVol('+setvol+');';
+    return code;
+    };
+Blockly.Arduino.mp3_Setup = function () {
+    var dropdown_pin1 = Blockly.Arduino.valueToCode(this, 'TX', Blockly.Arduino.ORDER_ATOMIC);
+    var dropdown_pin2 = Blockly.Arduino.valueToCode(this, 'RX', Blockly.Arduino.ORDER_ATOMIC);
+    Blockly.Arduino.definitions_['define_MP3Serial'] = '#include "SerialMP3Player.h"';
+    Blockly.Arduino.definitions_['define_PIN'] = '#define TX '+ dropdown_pin1 + '\n#define RX ' + dropdown_pin2 + '';
+    Blockly.Arduino.definitions_['define_SerialMP3Player'] = 'SerialMP3Player mp3(RX,TX);';
+    Blockly.Arduino.setups_['setup_MP3Player'] ='Serial.begin(9600);\n'
+  +'  mp3.begin(9600);\n'        
+  +'  delay(500);\n'             
+  +'  mp3.sendCommand(CMD_SEL_DEV, 0, 2);\n'   
+  +'  delay(500);';
+    var code='';
+    return code;
+    };
+
+Blockly.Arduino.mp3_control = function () {
+    var code = this.getFieldValue('mp3_Control');
+    return code;
+    };
+Blockly.Arduino.mp3_control2 = function () {
+    var file = Blockly.Arduino.valueToCode(this, 'file', Blockly.Arduino.ORDER_ATOMIC);
+    var code = 'mp3.play('+file+');\n'
+    return code;
+    };
+/*Blockly.Arduino.mp3_PalyByName = function () {
+    var songName = Blockly.Arduino.valueToCode(this, 'songName', Blockly.Arduino.ORDER_ATOMIC);
+    var code = 'mp3.playPF(01,'+songName+');\n'
+    return code;
+    };*/
+
+
+
+
